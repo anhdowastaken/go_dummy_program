77c6b2acec94a05fa701ad1649196e0ba200846e
And Do <doducanh2710@gmail.com>
2018-06-05 09:55:58 +0700

    Update script
    Signed-off-by: Anh Do <doducanh2710@gmail.com>

--- build.sh
+++ build.sh
@@ -9,6 +9,5 @@ if [[ ! -d "$GOPATH/pkg/" ]]; then
     mkdir -p "$GOPATH/pkg/"
 fi
 
-go get -v github.com/dq2nd/go_second_dummy_lib
 go build -o bin/go_dummy_program github.com/dq2nd/go_dummy_program
 
---
+++ get.sh
@@ -0,0 +1,5 @@
+GOPATH="$(pwd)"
+export GOPATH=$GOPATH
+
+go get -v github.com/dq2nd/go_second_dummy_lib
+
